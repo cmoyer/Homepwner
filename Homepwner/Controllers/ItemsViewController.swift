4892469701bb1557c5ad4a3d85d290cab4df2166
Chad Moyer <c.d.moyer521@gmail.com>
2017-04-12 20:34:36 -0400

    Set up the table view with the correct numberOfRowsInSection and cellForRowAt by using the reusable cells.

--- Homepwner/Controllers/ItemsViewController.swift
+++ Homepwner/Controllers/ItemsViewController.swift
@@ -12,4 +12,20 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
 }
